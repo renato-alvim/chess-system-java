81013906abb7d64b6dda92899b07fea62ddd8584
Renato Alvim <renatoalvim666@gmail.com>
2021-04-06 17:23:40 -0300

    Primeira Classe: Position

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import BoardGame.Position;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Position p = new Position(3,5);
+
+        System.out.println(p);
     }
 }
