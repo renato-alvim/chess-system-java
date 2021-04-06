cc2cc721c71e0f57968546a1f348dfee533355b2
Renato Alvim <renatoalvim666@gmail.com>
2021-04-06 18:16:52 -0300

    Camada Chess e printando o Board

--- src/Application/App.java
+++ src/Application/App.java
@@ -1,14 +1,14 @@
-import BoardGame.Board;
+package Application;
+
 import BoardGame.Position;
 import Chess.ChessMatch;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-       ChessMatch chessMatch = new ChessMatch();
-       UI.printboard(chessMatch.getPieces());
+        ChessMatch chessmatch = new ChessMatch();
+        UI.printBoard(chessmatch.getPieces());
 
-       
-        
     }
+    
 }
