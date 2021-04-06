4d53f21297bbf7e288b87c0f926c77fe63ce183c
Renato Alvim <renatoalvim666@gmail.com>
2021-04-06 19:04:30 -0300

    Programação defensiva e classe BoardException

---
+++ src/BoardGame/BoardExeption.java
@@ -0,0 +1,12 @@
+package BoardGame;
+
+public class BoardExeption extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public BoardExeption(String message) {
+        super(message);
+    }
+
+    
+    
+}
--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -30,12 +30,18 @@ public class ChessMatch {
 
     private void initialSetup()
     {
-        board.placePiece(new Rook(board,Color.WHITE),new Position(0,0));
-        board.placePiece(new Rook(board,Color.WHITE),new Position(7,7));
-        board.placePiece(new Rook(board,Color.WHITE),new Position(0,7));
+        board.placePiece(new Rook(board,Color.BLACK),new Position(0,0));
+        board.placePiece(new Rook(board,Color.BLACK),new Position(0,7));
+        board.placePiece(new King(board,Color.BLACK),new Position(0,4));
+
+
         board.placePiece(new Rook(board,Color.WHITE),new Position(7,0));
+        board.placePiece(new Rook(board,Color.WHITE),new Position(7,7));
         board.placePiece(new King(board,Color.WHITE),new Position(7,4));
-        board.placePiece(new King(board,Color.WHITE),new Position(0,4));
+
+        board.placePiece(new King(board,Color.WHITE),new Position(0,0));
+
+
     }
 
 }
